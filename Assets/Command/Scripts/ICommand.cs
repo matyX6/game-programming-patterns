1e2fe59e4fdcdcd74e32bb205614ecb9f9f48d22
DESKTOP-DJ3MO95\matyX6 <matyX6@gmail.com>
2020-04-04 16:06:30 +0200

    command undo redo core implementation

---
+++ Assets/Command/Scripts/ICommand.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Assets.Command.Scripts
+{
+    public class MoveUnitCommand
+    {
+        private Transform _unit;
+        private float _x;
+        private float _y;
+
+        public MoveUnitCommand(Transform unit, float x, float y)
+        {
+            _unit = unit;
+            _x = x;
+            _y = y;
+        }
+
+        public void Execute()
+        {
+            _unit.position = new Vector3(_x, _y);
+        }
+
+        public void Redo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
+
---
+++ Assets/Command/Scripts/InputHandler.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Assets.Command.Scripts
+{
+    class InputHandler : MonoBehaviour
+    {
+        [SerializeField] float _offsetValue = 1f;
+
+        private void Update()
+        {
+            MoveUnitCommand command = HandleInput();
+
+            if (command != null)
+            {
+                command.Execute();
+            }
+        }
+
+        private MoveUnitCommand HandleInput()
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                float destY = transform.position.y + _offsetValue;
+                return new MoveUnitCommand(transform, transform.position.x, destY);
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                float destY = transform.position.y - _offsetValue;
+                return new MoveUnitCommand(transform, transform.position.x, destY);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                float destX = transform.position.x - _offsetValue;
+                return new MoveUnitCommand(transform, destX, transform.position.y);
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                float destX = transform.position.x + _offsetValue;
+                return new MoveUnitCommand(transform, destX, transform.position.y);
+            }
+
+            return null;
+        }
+    }
+}
