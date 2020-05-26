9bdf0b5bbb6ffc76d2f5816279d8417b2a0d97f2
DESKTOP-DJ3MO95\matyX6 <matija.jambresic@gamesrevolted.net>
2020-05-26 15:32:17 +0200

    lens working first step

---
+++ Assets/SunFlare.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SunFlare : MonoBehaviour
+{
+    public float offset;
+    public Transform _transform;
+
+
+    private void Update()
+    {
+        Vector3 difference = Camera.main.transform.position - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+    }
+}
